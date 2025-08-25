74a978578ed743ec9ea7a5c564400cee102ef78c
sarank1634 <saravanakumar1634@gmail.com>
2025-08-25 15:29:30 +0530

    chapter one is completed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-const { Socket } = require('dgram')
 const ws = require('ws')
 const server = new ws.Server({ port: '3000' })
 
-server.on('connection', Socket => {
-    Socket.on('message', message => {
+server.on('connection', socket => {
+    socket.on('message', message => {
         const b = Buffer.from(message)
         console.log(b.toString())
-        Socket.send(`${message}`)
+        socket.send(`${message}`)
     })
 })
